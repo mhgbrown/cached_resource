3f7968fcff83a2b4b218406156a7382f02ae083a
Morgan Brown <brown.mhg@gmail.com>
2011-10-13 11:53:00 -0700

    Initial rspec testing env setup

---
+++ spec/cached_resource/caching.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe CachedResource do
+  pending "write it"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'active_resource'
+require 'active_support'
+
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+require 'cached_resource'
+
+
+RSpec.configure do |config|
+  # nada
+end
