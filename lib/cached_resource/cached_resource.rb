19542032a2db0a5b1daf56bdbf726c25cc36a266
Morgan Brown <brown.mhg@gmail.com>
2011-11-09 16:36:46 -0800

    Fix a problem with cached_resource that would cause cached resource options to not be set correctly

--- lib/cached_resource/cached_resource.rb
+++ lib/cached_resource/cached_resource.rb
@@ -5,22 +5,24 @@ module CachedResource
   module Model
     extend ActiveSupport::Concern
 
-    module ClassMethods
+    included do
+      class << self
+        attr_accessor :cached_resource
 
-      # initialize cached resource or retrieve the current cached resource configuration
-      def cached_resource(options={})
-        defined?(@@cached_resource) && @@cached_resource || setup_cached_resource!(options)
+        # initialize cached resource or retrieve the current cached resource configuration
+        def cached_resource(options={})
+          defined?(@cached_resource) && @cached_resource || setup_cached_resource!(options)
+        end
+
+        # setup cached resource for this class by creating a new configuration
+        # and establishing the necessary methods.
+        def setup_cached_resource!(options)
+          @cached_resource = CachedResource::Configuration.new(options)
+          send :include, CachedResource::Caching
+        end
+
+
       end
-
-      private
-
-      # setup cached resource for this class by creating a new configuration
-      # and establishing the necessary methods.
-      def setup_cached_resource!(options)
-        @@cached_resource = CachedResource::Configuration.new(options)
-        send :include, CachedResource::Caching
-      end
-
     end
 
   end
